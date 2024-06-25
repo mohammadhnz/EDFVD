1ebba963addf18438e93a1b4309e893452ed5052
Mohammadali Hosseinnezhad <hmohammadali2013@gmail.com>
2024-06-25 19:25:05 +0330

    feat: add edf vd schedule

--- models/simulator.py
+++ models/simulator.py
@@ -1,7 +1,45 @@
+from typing import List
+
 import configs
+from models import HighCriticalityTask, LowCriticalityTask
 
 
 class Simulator:
     def __init__(self):
         self.mode = configs.Mode.NORMAL
-        self.queue = []
+        self.high_criticality_queue: List[HighCriticalityTask] = []
+        self.low_criticality_queue: List[LowCriticalityTask] = []
+        self.current_time = 0
+        self.edf_vd_x = self._get_edf_vd_x()
+
+    def _get_edf_vd_x(self):
+        u_lo_lo, u_lo_hi = 0, 0
+        for high_critical_task in self.high_criticality_queue:
+            u_lo_hi += high_critical_task.little_computation_time / high_critical_task.period
+        for low_criticality_task in self.low_criticality_queue:
+            u_lo_lo += low_criticality_task.computation_time / low_criticality_task.period
+        edf_vd_x = u_lo_hi / (1 - u_lo_lo)
+        return round(edf_vd_x, 2)
+
+    def execute(self):
+        while True:
+            self._schedule_task()
+            self.current_time += 1
+
+    def _schedule_task(self, current_time):
+        self._handle_done_tasks()
+        tasks = self._get_tasks_by_deadline_ordering()
+
+    def _get_tasks_by_deadline_ordering(self):
+        if self.mode == configs.Mode.NORMAL:
+            return sorted(self.high_criticality_queue + self.low_criticality_queue, key=lambda task: task.deadline)
+        return sorted(self.high_criticality_queue, key=lambda task: task.period)
+
+    def _filter_eligible_tasks(self):
+        pass
+
+    def assign_to_core(self):
+        pass
+
+    def _handle_done_tasks(self):
+        pass
