2e08fa2a7776143bfc1f2ae1a951155efe239279
farhadesmzdh <farhadesmzdh@gmail.com>
2024-06-25 21:18:11 +0330

    feat: add core utilization restriction to generated tasks

--- algorithms/uunifast.py
+++ algorithms/uunifast.py
@@ -3,22 +3,15 @@ from models import HighCriticalityTask, LowCriticalityTask
 import random
 
 
-def generate_task(total_utilization, number_of_tasks, ratio) -> (List[HighCriticalityTask], List[LowCriticalityTask]):
+def generate_task(core_utilization, number_of_cores, number_of_tasks, ratio) -> (List[HighCriticalityTask], List[LowCriticalityTask]):
     utilizations = []
-    sum_u = total_utilization
-    for i in range(number_of_tasks - 1):
-        next_sum_u = sum_u * random.random() ** (total_utilization / (number_of_tasks - i))
-        utilizations.append(sum_u - next_sum_u)
-        sum_u = next_sum_u
-    utilizations.append(sum_u)
-
-    sets = []
-    if all(ut <= 1 for ut in utilizations):
-        sets.extend(utilizations)
+    for i in range(number_of_cores):
+        new_utilizations = _uunifast_algorithm(core_utilization, number_of_tasks // number_of_cores)
+        utilizations.extend(new_utilizations)
 
     number_of_hc_tasks = int(number_of_tasks * ratio)
-    hc_utilization = sets[:number_of_hc_tasks]
-    lc_utilization = sets[number_of_hc_tasks:]
+    hc_utilization = utilizations[:number_of_hc_tasks]
+    lc_utilization = utilizations[number_of_hc_tasks:]
 
     hc_tasks = []
     for u in hc_utilization:
@@ -37,3 +30,18 @@ def generate_task(total_utilization, number_of_tasks, ratio) -> (List[HighCritic
 
     return hc_tasks, lc_tasks
 
+
+def _uunifast_algorithm(core_utilization, number_of_tasks) -> List:
+    utilizations = []
+    sum_u = core_utilization
+    for i in range(number_of_tasks - 1):
+        next_sum_u = sum_u * random.random() ** (core_utilization / (number_of_tasks - i))
+        utilizations.append(sum_u - next_sum_u)
+        sum_u = next_sum_u
+    utilizations.append(sum_u)
+
+    sets = []
+    if all(ut <= core_utilization for ut in utilizations):
+        sets.extend(utilizations)
+
+    return sets
